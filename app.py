5c12e1f883dba4d778c5a136156f62b6b5d9eecb
emfamiliaminasgerais <emfamiliaminasgerais@gmail.com>
2026-09-14 14:34:21 -0300

    Add since_friday default filter and local timezone date calculation

--- app.py
+++ app.py
@@ -180,7 +180,8 @@ INVALID_WORDS = {
     'federal', 'macaé', 'macae', 'matias', 'taquaraçu', 'taquaracu', 'sabará', 'sabara', 'neves', 'lagoas',
     'moura', 'leopoldina', 'ibirité', 'ibiritê', 'confins', 'bocaiúva', 'bocaiuva', 'oliveira', 'santa clara',
     'belo horizonte', 'uberlândia', 'uberlandia', 'juiz de fora', 'contagem', 'betim', 'montes claros',
-    'ipatinga', 'governador valadares', 'sete lagoas', 'deus', 'senhor', 'jesus', 'cristo', 'bispo', 'pastor'
+    'ipatinga', 'governador valadares', 'sete lagoas', 'deus', 'senhor', 'jesus', 'cristo', 'bispo', 'pastor',
+    'dr', 'doutor', 'prof', 'professor', 'professora', 'deputado', 'prefeito', 'vereador', 'bolsonaro', 'caiado', 'chagas'
 }
 
 def is_valid_person_name(name_str):
